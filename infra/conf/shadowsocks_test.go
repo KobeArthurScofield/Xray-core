ab9f22b614068d2904b8805374e115fd5edef6ca
Kobe Arthur Scofield <26771058+KobeArthurScofield@users.noreply.github.com>
2024-09-16 00:47:10 +0800

    Remove freedom timeout

--- infra/conf/shadowsocks_test.go
+++ infra/conf/shadowsocks_test.go
@@ -29,7 +29,7 @@ func TestShadowsocksServerConfigParsing(t *testing.T) {
 						Password:   "xray-password",
 					}),
 				}},
-				Networks: []net.Network{net.Network_TCP},
+				Network: []net.Network{net.Network_TCP},
 			},
 		},
 	})
