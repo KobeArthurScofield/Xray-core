9fcbf388677a19bf35c268d901f60f2552cacccd
Kobe Arthur Scofield <26771058+KobeArthurScofield@users.noreply.github.com>
2024-09-19 00:59:05 +0800

    Supressor

--- testing/scenarios/dns_test.go
+++ testing/scenarios/dns_test.go
@@ -35,7 +35,8 @@ func TestResolveIP(t *testing.T) {
 					{
 						Type:   dns.DomainMatchingType_Full,
 						Domain: "google.com",
-						Ip:     [][]*byte{net.NewIPOrDomain(dest.Address).AsAddress()},
+						Ip:     []*byte(dest.Address),
+						//Ip:     []*byte(net.NewIPOrDomain(dest.Address).AsAddress()),
 					},
 				},
 			}),
